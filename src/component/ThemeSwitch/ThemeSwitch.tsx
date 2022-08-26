66cad027a0a5d950748fa4215a73783d06815f94
Fung Mak <fung.mak@6317.io>
2022-08-26 17:22:27 +0800

    added dark theme

---
+++ src/component/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Switch } from 'antd'
+import { useSelector } from 'react-redux'
+import { StateModel } from '../../store/model/state.model'
+import { changeTheme, THEME } from '../../util/ChangeTheme'
+import { store } from '../../store'
+import { StateAction } from '../../store/reducer'
+
+export function ThemeSwitch (): JSX.Element {
+  const state = useSelector((state: StateModel) => state)
+
+  return <>
+    <Switch defaultChecked={state.theme === THEME.DARK} onChange={(value) => {
+      const theme = THEME[value ? 'DARK' : 'LIGHT']
+      void changeTheme(theme)
+      store.dispatch({
+        type: StateAction.SET_THEME,
+        data: THEME[value ? 'DARK' : 'LIGHT']
+      })
+    }} />
+    </>
+}
